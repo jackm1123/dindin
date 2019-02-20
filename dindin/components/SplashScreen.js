f33163d0f296d516a31f87d5e44bb348c1aacdcd
Jack McManus <jackmcmanus2@gmail.com>
2019-02-20 00:56:48 -0500

    added to splash screen1

--- dindin/components/SplashScreen.js
+++ dindin/components/SplashScreen.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import {View, StyleSheet, ImageBackground, TouchableOpacity, Text, Dimensions} from 'react-native'
+import {Animated, View, StyleSheet, ImageBackground, Image, TouchableOpacity, Text, Dimensions} from 'react-native'
 import { Constants } from 'expo'
-//import {widthPercentageToDP as wp, heightPercentageToDP as hp, listenOrientationChange as loc, removeOrientationListener as rol} from 'react-native-responsive-screen';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp, listenOrientationChange as loc, removeOrientationListener as rol} from 'react-native-responsive-screen';
 
 
 
@@ -10,12 +10,23 @@ import { Constants } from 'expo'
 
 export default class SplashScreen extends React.Component{
   constructor(props){
-    super(props)
+    super(props);
   }
+
 
   render(){
     return(
       <View style={styles.backgroundContainer}>
+        <Image style={styles.circles} source={require('../assets/ovals2.png')}>
+        </Image>
+        <Image style={styles.home} source={require('../assets/house2.png')}>
+        </Image>
+        
+        {/*<Animated.View>*/}
+          <Image style={styles.pinkface} source={require('../assets/pinkface2.png')}>
+          </Image>
+        {/*</Animated.View>*/}
+
         <TouchableOpacity onPress={()=>this.props.navigation.navigate('Home')}>
           <View>
             <ImageBackground style={styles.buttonImage} source={require('../assets/Button.png')}>
@@ -23,6 +34,9 @@ export default class SplashScreen extends React.Component{
             </ImageBackground>
           </View>
         </TouchableOpacity>
+      
+
+        
       </View>
     )
   }
@@ -36,8 +50,8 @@ const styles = StyleSheet.create(
           paddingTop: Constants.statusBarHeight,
           height: undefined,
           width: undefined,
-          justifyContent: 'flex-end'
-
+          justifyContent: 'flex-end',
+          alignItems: 'center',
         },
         buttonImage:{
           width: wp('100%'),
@@ -52,7 +66,41 @@ const styles = StyleSheet.create(
           fontSize: hp('2.4%'),
           fontFamily: 'Helvetica',
           justifyContent: 'space-evenly'
-        }
+        },
+        circles:{
+          position: 'absolute',
+          bottom: hp('45%'),
+          width: hp('40%'),
+          height: hp('40%'),
+        },
+        home:{
+          position: 'absolute',
+          bottom: hp('59%'),
+          width: hp('12%'),
+          height: hp('12%'),
+        },
+        pinkface:{
+          position: 'absolute',
+          bottom: hp('53%'),
+          width: hp('12%'),
+          height: hp('13.5%'),
+          right: wp('70%'),
+        },
+        purpleface:{
+          position: 'absolute',
+          bottom: hp('53%'),
+          width: hp('12%'),
+          height: hp('13.5%'),
+          right: wp('70%'),
+        },
+
+//https://www.lynda.com/React-Native-tutorials/Creating-animation-loop/560343/672458-4.html
+//https://facebook.github.io/react-native/docs/animations
+
+
+
+
+
     }
 )
 
