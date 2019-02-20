2bd2e06613728b3d3f54d5642e46a623c2e49579
Jack McManus <jackmcmanus2@gmail.com>
2019-02-20 16:57:02 -0500

    added animation to splashscreen

--- dindin/components/SplashScreen.js
+++ dindin/components/SplashScreen.js
@@ -1,31 +1,82 @@
+/*
+Sources used:
+https://www.lynda.com/React-Native-tutorials/Creating-animation-loop/560343/672458-4.html
+https://facebook.github.io/react-native/docs/animations
+*/
 import React from 'react'
 import {Animated, View, StyleSheet, ImageBackground, Image, TouchableOpacity, Text, Dimensions} from 'react-native'
 import { Constants } from 'expo'
 import {widthPercentageToDP as wp, heightPercentageToDP as hp, listenOrientationChange as loc, removeOrientationListener as rol} from 'react-native-responsive-screen';
 
+/* Making Fade Animation View */
+class FadeInView extends React.Component {
+  state = {
+    fadeAnim: new Animated.Value(1),  // Initial value for opacity: 1
+  }
+
+  /* animateFade calls itself over and over as param of start()*/
+  animateFade = (opacity = 1) => {
+    Animated.timing(
+      this.state.fadeAnim,
+      {
+        toValue: opacity,
+        duration: 1000,
+      }
+    ).start(() => { this.animateFade(1 ^ opacity)}) // XOR 1 with current opacity to alternate 0 and 1
+  }
+
+  async componentDidMount(){
+    this.animateFade()
+  }
+
+  render() {
+    let { fadeAnim } = this.state
 
 
-
+/* {this.props.children} is used to display whatever you include between
+the opening and closing tags when invoking a component
+...this.props.style uses ... to expand the array of this.props,style into a list.
+this.props.style is used for inheriting from classes around it*/
+    return (
+      <Animated.View style={{...this.props.style, opacity: fadeAnim}}>
+        {this.props.children}
+      </Animated.View>
+    );
+  }
+}
 
 
 export default class SplashScreen extends React.Component{
   constructor(props){
-    super(props);
+    super(props)
   }
 
 
   render(){
+
     return(
       <View style={styles.backgroundContainer}>
         <Image style={styles.circles} source={require('../assets/ovals2.png')}>
         </Image>
         <Image style={styles.home} source={require('../assets/house2.png')}>
         </Image>
-        
-        {/*<Animated.View>*/}
-          <Image style={styles.pinkface} source={require('../assets/pinkface2.png')}>
+
+        <FadeInView style={styles.pinkface}>
+          <Image source={require('../assets/pinkface2.png')}>
           </Image>
-        {/*</Animated.View>*/}
+        </FadeInView>
+        <FadeInView style={styles.purpleface}>
+          <Image source={require('../assets/purpleface.png')}>
+          </Image>
+        </FadeInView>
+        <FadeInView style={styles.greenface}>
+          <Image source={require('../assets/greenface.png')}>
+          </Image>
+        </FadeInView>
+
+
+        <Text style={styles.title}>DinDin</Text>
+        <Text style={styles.description}>Connecting food lovers</Text>
 
         <TouchableOpacity onPress={()=>this.props.navigation.navigate('Home')}>
           <View>
@@ -35,8 +86,6 @@ export default class SplashScreen extends React.Component{
           </View>
         </TouchableOpacity>
       
-
-        
       </View>
     )
   }
@@ -88,19 +137,36 @@ const styles = StyleSheet.create(
         },
         purpleface:{
           position: 'absolute',
-          bottom: hp('53%'),
+          bottom: hp('70%'),
           width: hp('12%'),
           height: hp('13.5%'),
-          right: wp('70%'),
+          right: wp('18%'),
         },
-
-//https://www.lynda.com/React-Native-tutorials/Creating-animation-loop/560343/672458-4.html
-//https://facebook.github.io/react-native/docs/animations
-
-
-
-
-
+        greenface:{
+          position: 'absolute',
+          bottom: hp('40%'),
+          width: hp('12%'),
+          height: hp('13.5%'),
+          right: wp('24%'),
+        },
+        title:{
+          fontFamily: 'Helvetica',
+          fontSize: hp('4%'),
+          color: '#353535',
+          textAlign: 'center',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          marginTop: hp('3%'),
+        },
+        description:{
+          opacity: 0.5,
+          fontFamily: 'Helvetica',
+          fontSize: hp('2%'),
+          color: '#000000',
+          textAlign: 'center',
+          marginBottom: hp('20%'),
+          marginTop: hp('1%'),
+        },
     }
 )
 
